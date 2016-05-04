086fcedf7e4df979bfd5edbbed66c87c5e8caeb7
meg768 <magnus@egelberg.se>
2016-05-04 20:46:29 +0200

    -

--- app.js
+++ app.js
@@ -1,77 +1,14 @@
 var Downloader = require('./src/downloader.js');
 var args       = require('minimist')(process.argv.slice(2));
 var fs         = require('fs');
+var App        = require('./src/app.js');
 
-var App = module.exports = function() {
 
-	var _this = this;
-	
-	_this.config = JSON.parse(fs.readFileSync('./config.json'));
-	
-	_this.scheduleDownload = function(folder) {
-		var downloader = new Downloader(require('./src/stocks.js').symbols, folder);
-		downloader.scheduleDownload();
-		
-	};
-	
-	function init() {
-		
-	}
-}
-
-function main() {
+function run() {
 	var app = new App();
-
-	if (args.stderr == undefined) {
-		if (app.config.logs && app.config.logs.stderr) {
-			args.stderr = app.config.logs.stderr;			
-		}		
-	}
-
-	if (args.stdout == undefined) {
-		if (app.config.logs && app.config.logs.stdout) {
-			args.stdout = app.config.logs.stdout;			
-		}		
-	}
-
-
-	
-	// Redirect stdout?
-	if (args.stdout) {
-		var access = fs.createWriteStream(args.stdout);
-		process.stdout.write = access.write.bind(access);
-		
-	}
-
-	// Redirect stderr?
-	if (args.stderr) {
-		var access = fs.createWriteStream(args.stderr);
-		process.stderr.write = access.write.bind(access);
-	
-		// Make sure we catch uncaught exceptions
-		process.on('uncaughtException', function(err) {
-			console.error((err && err.stack) ? err.stack : err);
-		});
-		
-
-	}
-
-	if (args.download) {
-
-		if (args.folder == undefined) {
-			args.folder = app.config.stockFolder;
-		}
-
-		if (args.folder == undefined) {
-			console.error('The --folder option must be specified.');
-		}
-		else {
-			app.scheduleDownload(args.folder);
-			
-		}
-	}
+	app.run(args);
 
 }	
 
 
-main();
+run();
---
+++ src/app.js
@@ -0,0 +1,68 @@
+var Downloader = require('./downloader.js');
+var fs         = require('fs');
+
+var App = module.exports = function() {
+
+	var _this = app = this;
+	
+	_this.config = JSON.parse(fs.readFileSync('./config.json'));
+	
+
+	_this.scheduleDownload = function(folder) {
+		var downloader = new Downloader(require('./stocks.js').symbols, folder);
+		downloader.scheduleDownload();
+		
+	};
+	
+	_this.run = function(args) {
+	
+		if (args.stderr == undefined) {
+			if (app.config.logs && app.config.logs.stderr) {
+				args.stderr = app.config.logs.stderr;			
+			}		
+		}
+	
+		if (args.stdout == undefined) {
+			if (app.config.logs && app.config.logs.stdout) {
+				args.stdout = app.config.logs.stdout;			
+			}		
+		}
+	
+	
+		
+		// Redirect stdout?
+		if (args.stdout) {
+			var access = fs.createWriteStream(args.stdout);
+			process.stdout.write = access.write.bind(access);
+			
+		}
+	
+		// Redirect stderr?
+		if (args.stderr) {
+			var access = fs.createWriteStream(args.stderr);
+			process.stderr.write = access.write.bind(access);
+		
+			// Make sure we catch uncaught exceptions
+			process.on('uncaughtException', function(err) {
+				console.error((err && err.stack) ? err.stack : err);
+			});
+			
+	
+		}
+	
+		if (args.download) {
+	
+			if (args.folder == undefined) {
+				args.folder = app.config.stockFolder;
+			}
+	
+			if (args.folder == undefined) {
+				console.error('The --folder option must be specified.');
+			}
+			else {
+				app.scheduleDownload(args.folder);
+				
+			}
+		}		
+	};
+};
