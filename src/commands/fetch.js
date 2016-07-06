232578103f2535b277d70f8ab90cb7c854c32203
meg768 <magnus@egelberg.se>
2016-07-06 17:47:38 +0200

    -

--- src/commands/fetch.js
+++ src/commands/fetch.js
@@ -1,15 +1,75 @@
-
-
 var fs          = require('fs');
 var sprintf     = require('yow').sprintf;
 var mkdir       = require('yow').mkdir;
+var isArray     = require('yow').isArray;
 var fileExists  = require('yow').fileExists;
 var Promise     = require('bluebird');
-var sqlite3     = require('sqlite3').verbose();
 
 var Module = module.exports = function(args) {
 
-	// rsync -avz pi@10.0.1.42:/home/pi/munch/data/downloads data
-	console.log('OK!')
+	function exec(cmd, args, options) {
+
+		if (options == undefined)
+			options = {};
+
+		if (!isArray(args)) {
+			args = [args];
+		}
+
+		function YES() {
+			console.log('Done');
+
+		};
+
+		function NO() {
+			console.log('Nope');
+
+		};
+
+		try {
+			var spawn = require('child_process').spawn;
+
+			//var cmd     = 'rsync';
+			//var args    = ['-avz', 'pi@10.0.1.42:/home/pi/munch/data/downloads', 'data'];
+			//var options = {};
+			var process = spawn(cmd, args, options);
+
+			process.stderr.on('data', function (data) {
+				console.log('ERROR#', data.toString());
+			});
+
+			process.stdout.on('data', function (data) {
+				console.log(data.toString());
+			});
+
+			if (process == null) {
+				NO();
+			}
+			else {
+				process.on('error', function() {
+					NO();
+				});
+
+				process.on('close', function() {
+					YES();
+				});
+			}
+
+			return _process = process;
+		}
+		catch (error) {
+			console.log(error);
+			NO(error);
+		}
+
+	}
+
+	this.run = function() {
+		//exec('ls', 'data');
+		//exec('rsync', ['-avz', 'pi@10.0.1.42:/home/pi/munch/data/downloads/quotes/2016-07-05', 'q/downloads/quotes/2016-07-05']);
+		exec('rsync', ['-avz', 'pi@10.0.1.42:/home/pi/munch/data/downloads', 'data']);
+		// rsync -avz pi@10.0.1.42:/home/pi/munch/data/downloads data
+
+	};
 
 };
--- src/scripts/console-prefix.js
+++ src/scripts/console-prefix.js
@@ -1,35 +1,35 @@
 var prefix = module.exports = function(fn) {
-	
+
 	var funcs = {
 		log: console.log,
 		error: console.error,
 		warn: console.warn,
 		info: console.info
 	};
-	
+
 	for (var key in funcs) {
 		console[key] = function() {
 			var output = '';
 			var prefix = typeof fn == 'function' ? fn() : fn;
-			
+
 			if (typeof arguments[0] == 'string') {
 				arguments[0].split('\n').forEach(function(line) {
 
 					if (output != '')
 						output += '\n';
-						
-					output += prefix + line; 	
+
+					output += prefix + line;
 				});
 			}
 			else {
 				output = prefix + arguments[0];
 			}
-			
+
 			arguments[0] = output;
-			
+
 			funcs[key].apply(console, arguments);
-			
+
 		}
 	}
-	
+
 }
