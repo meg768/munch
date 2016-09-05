690584f2a49d41a8bad39c47d455c51de0e70721
meg768 <magnus@egelberg.se>
2016-09-05 22:53:34 +0200

    -

--- src/algos/sample.js
+++ src/algos/sample.js
@@ -2,10 +2,10 @@ var fs         = require('fs');
 var sprintf    = require('yow').sprintf;
 
 var Workday = function(engine, config) {
-	
+
 	var _state = 'idle';
-	
-	
+
+
 };
 
 
@@ -18,10 +18,10 @@ var Module = module.exports = function(engine) {
 		engine.stocks  - contains information about each company
 		engine.symbols - array containing all symbols
 	*/
-	
+
 	var workday = null;
 	var config  = engine.config;
-	
+
 	this.onData = function() {
 		var data = engine.data;
 		var symbol = config.symbol;
@@ -30,31 +30,31 @@ var Module = module.exports = function(engine) {
 		// Build up the key (HH:MM)
 		var dateKey = sprintf('%04d-%02d-%02d', engine.time.getFullYear(), engine.time.getMonth()+1, engine.time.getDate());
 		var timeKey = sprintf('%02d:%02d', engine.time.getHours(), engine.time.getMinutes());
-		
-		
+
+
 		if (data[symbol]) {
-			console.log(sprintf('%s %s %s %f', dateKey, timeKey, symbol, data[symbol].close));
+//			console.log(sprintf('%s %s %s %f', dateKey, timeKey, symbol, data[symbol].close));
 		}
 	}
 
 	this.onStartOfAlgorithm = function() {
 		console.log('Started algorithm', engine.time);
-		
+
 		workday = new Workday(engine, config);
 	}
 
 	this.onEndOfAlgorithm = function() {
-		console.log('Ended algorithm', engine.time);		
-		
+		console.log('Ended algorithm', engine.time);
+
 	}
 
 	this.onStartOfDay = function() {
-		console.log('Start of day', engine.time);		
-		
+		console.log('Start of day', engine.time);
+
 	}
-	
+
 	this.onEndOfDay = function() {
-		console.log('End of day', engine.time);		
+		console.log('End of day', engine.time);
 	}
 
 
