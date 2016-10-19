de7d793a867ea7fd7110313ec28fc58f1e0fc3ad
meg768 <magnus@egelberg.se>
2016-10-19 22:20:00 +0200

    -

--- scripts/download-quotes.sh
+++ scripts/download-quotes.sh
@@ -7,4 +7,4 @@ DATESTAMP=$(date +"%Y-%m-%d-%H-%M")
 exec >> ${LOCATION}/../logs/${DATESTAMP}.log
 exec 2>&1
 
-cd ${LOCATION}/.. && node app.js --run download-quotes --count 10 --days 10
+cd ${LOCATION}/.. && node app.js --run download-quotes --count 10 --days 14
