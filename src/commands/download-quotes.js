58de4ab0613e3339ef835549ab6d3582311201fe
meg768 <meg768@users.noreply.github.com>
2019-08-12 00:18:56 +0200

    -

--- src/commands/download-quotes.js
+++ src/commands/download-quotes.js
@@ -594,7 +594,7 @@ var Module = new function() {
 				.then(() => {
 					var now = new Date();
 					console.log('Symbol %s finished in %d seconds.', (timestamp - now) / 1000);
-					
+
 					if (quotes)
 						return updateStock(symbol);
 					else
